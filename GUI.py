173c727e3d99fecf69dcad2239b70a0671a0629e
CriPiffe <94227878+CriPiffe@users.noreply.github.com>
2022-08-22 12:47:58 +0200

    minor fix

--- GUI.py
+++ GUI.py
@@ -1,61 +1,169 @@
 import tkinter as tk
 import os
 import chapter
+import logger
+
+
+log = logger.logger()
+
+
+def checkSetting(entry, default):
+    val = entry.get()
+    if val.isdigit():
+        val = int(val)
+    else:
+        val = default
+    return val
 
 def start():
+    #check input parameters for page slicing
+    minimumsize = checkSetting(minsize, 2500)
+    rowDistance= checkSetting(scanDistance, 20)
+    border = checkSetting(bordersize, 60)
+    space = checkSetting(spacing, 5)
+
+    settings =[minimumsize, rowDistance, border, space]
+
+    # check path input
     orPath = originPath.get()
     svPath = savePath.get()
     
     #check origin folder
     if len(orPath) <=3:
-        communicationEntry.insert(tk.END, "\nType a possible path, this is too short!")
+        log.addTextToLabel(communicationEntry, 
+            "Type a possible path, this is too short!"
+        )   
         return
     else:
         orPath = os.path.abspath(originPath.get())
     
+    if not orPath.isdir():
+        log.addTextToLabel(communicationEntry, 
+            "This path does not exist!"
+        )  
+
     #check save folder
     if len(svPath) == 0:
-        chapter.chapter(originPath=orPath).brutalSlice()
-        communicationEntry.insert(tk.END, "\nfinished!")
+        svPath = None
         return
     elif len(svPath) <= 3:
-        communicationEntry.insert(tk.END, "\nType a possible path, this is too short!")
+        log.addTextToLabel(communicationEntry, 
+            "Type a possible path, this is too short!"
+        )  
         return
     else:
         svPath = os.path.abspath(savePath.get())
-
     
-    chapter.chapter(originPath=orPath, savePath=svPath).brutalSlice()
-    communicationEntry.insert(tk.END, "\nfinished!")
-    
+    chapter.chapter(originPath=orPath, savePath=svPath).brutalSlice(offset=settings)
+    log.addTextToLabel(communicationEntry, 
+        "finished!", separator='\n\n'
+    ) 
 
 
-
-
-
+## window spec
 window = tk.Tk()
-window.geometry("600x200")
+window.geometry('1000x600+0+0')
 window.title("Page Slicer 3000")
+window.resizable(False, False)
+##window.iconbitmap(pathToIcon)
 
+######## window frames
+# main frame
+leftFrame = tk.Frame(master=window, width=500, height=600,
+    #highlightbackground="blue", highlightthickness=2
+)
+leftFrame.pack(side=tk.LEFT)
+leftFrame.pack_propagate(0)
+
+rightFrame = tk.Frame(master=window, width=500, height=600, 
+    #highlightbackground="blue", highlightthickness=2
+)
+rightFrame.pack(side=tk.RIGHT)
+rightFrame.pack_propagate(0)
+
+#sub frame
+pathFrame = tk.Frame(master=leftFrame, highlightbackground="yellow", highlightthickness=2)
+pathFrame.pack(fill=tk.X)
+
+logFrame = tk.Frame(master=rightFrame, height=600, highlightbackground="yellow", highlightthickness=2)
+logFrame.pack(fill=tk.BOTH)
+
+settingFrame = tk.Frame(master=leftFrame, highlightbackground="yellow", highlightthickness=2)
+settingFrame.pack(fill=tk.X, pady=20)
+
+
+#path explain label
 explain = tk.Label(
+    master=pathFrame,
     text="Insert origin and save path"
-).pack(side=tk.TOP)
+).pack()
 
-originPath = tk.Entry(width=100)
-originPath.pack()
-savePath = tk.Entry(width=100)
-savePath.pack()
+#setting explain label
+settingLabel = tk.Label(
+    master=settingFrame,
+    text="Set all the wanted parameters"
+).pack()
 
-communicationEntry = tk.Entry(width=100)
-communicationEntry.pack()
+#communication log label
+communicationEntry = tk.Label(
+    master=logFrame,
+    height=600, 
+    anchor='n',
+    #highlightbackground="green", highlightthickness=2
+)
+communicationEntry.pack(fill=tk.BOTH)
 
+#path entry in path frame
+originPath = tk.Entry(
+    master=pathFrame
+)
+originPath.pack(fill=tk.X, pady=10)
+
+savePath = tk.Entry(
+    master=pathFrame
+)
+savePath.pack(fill=tk.X, pady=10)
+
+# setting entry
+minsizeFrame = tk.Frame(master=settingFrame)
+minsizeFrame.pack(fill=tk.X, pady=10)
+minsizeLabel = tk.Label(
+    master=minsizeFrame,
+    text="Minimum\npage size", highlightbackground="green", highlightthickness=2
+).pack(side=tk.LEFT)
+minsize = tk.Entry(
+    master=minsizeFrame
+)
+minsize.pack(fill=tk.X, side=tk.LEFT)
+#minsize.insert(tk.END,"Insert min size of the sliced page")
+
+scanDistance = tk.Entry(
+    master=settingFrame
+)
+scanDistance.pack(fill=tk.X, pady=10)
+#minsize.insert(tk.END,"Insert distance between consecutive row scan")
+
+bordersize = tk.Entry(
+    master=settingFrame
+)
+bordersize.pack(fill=tk.X, pady=10)
+#minsize.insert(tk.END,"Insert size for border check")
+
+spacing = tk.Entry(
+    master=settingFrame
+)
+spacing.pack(fill=tk.X, pady=10)
+#minsize.insert(tk.END,"Insert spacing for row check")
+
+#window button
 startButton = tk.Button(
+    master=settingFrame,
     text="Start!",
-    width=25,
-    height=5,
+    width=5,
+    height=2,
     command=start
 )
-startButton.pack(side=tk.BOTTOM)
+startButton.pack()
 
 
 if __name__ == "__main__":
--- chapter.py
+++ chapter.py
@@ -1,11 +1,12 @@
+from genericpath import isdir
 import cv2
 import numpy as np
 import os
 import slicer
 
 class chapter(object):
-    def __init__(self, originPath, savePath=None, nImage=0, totalHeigth=0, 
-                 pages=[], nPages=0, width=700, fileExt = ".jpg"):
+    def __init__(self, originPath, savePath=None, pages=[], 
+                    nPages=0, width=700, fileExt = ".jpg"):
         
         self.originPath = originPath
         
@@ -13,15 +14,20 @@ class chapter(object):
             pardir = os.path.abspath(os.path.join(originPath, os.pardir))
             os.makedirs(os.path.join(pardir, "slicedChapter"))
             self.savePath = os.path.abspath(os.path.join(pardir, "slicedChapter"))
+        elif not savePath.isdir():
+            os.makedirs(savePath)
+            self.savePath = savePath
         else:
             self.savePath = savePath
-        self.nImage = nImage
-        self.totalHeigth = totalHeigth
+        
         self.allPages = self.unificator(originPath)
         self.pages = pages
         self.nPages = nPages
         self.width = width
         self.fileExt = fileExt
+
+        self.__nImage = 0
+        self.__totalHeigth = 0
 
 
     def unificator(self, path):
@@ -32,15 +38,15 @@ class chapter(object):
             if filename.endswith('.jpg'):
                 imgTemp = cv2.imread(path + '\\' +filename)
                 img.append(imgTemp)
-                self.totalHeigth += imgTemp.shape[0]
-                self.nImage += 1
+                self.__totalHeigth += imgTemp.shape[0]
+                self.__nImage += 1
 
         self.width = imgTemp.shape[1]
-        allPages = np.zeros((self.totalHeigth, self.width, 3), dtype=np.uint8)
+        allPages = np.zeros((self.__totalHeigth, self.width, 3), dtype=np.uint8)
         allPages[:,:] = (255,255,255)
 
         hPrev = 0
-        for i in range(self.nImage):
+        for i in range(self.__nImage):
             hNext = img[i].shape[0]
             allPages[hPrev:hPrev+hNext, :, :3] = img[i]
             hPrev += hNext
@@ -55,7 +61,7 @@ class chapter(object):
         rowIndex = pageOffset
         startNewPage = 0
 
-        while rowIndex < self.totalHeigth - 200:
+        while rowIndex < self.__totalHeigth - 200:
             if(slicer.checkIfSlice(self.allPages[rowIndex,:], borderOffset=borderOffset, spacing=spacing)):
 
                 ##slice and create a new page
@@ -67,9 +73,10 @@ class chapter(object):
             else:
                 rowIndex += rowOffset
 
-        self.pages.append(slicer.slice(self.allPages, startNewPage, self.totalHeigth, self.width))
+        self.pages.append(slicer.slice(self.allPages, startNewPage, self.__totalHeigth, self.width))
         self.nPages += 1
         cv2.imwrite(self.savePath + "\\" + str(self.nPages).zfill(3) + self.fileExt,self.pages[self.nPages-1])
+        os.startfile(self.savePath)
 
             
 
